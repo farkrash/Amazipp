a0884b0b181639835951e560938f4f83b71e8d5c
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-04-11 16:58:30 +0300

    added objects to scene and scripts

---
+++ Amazipp/Assets/AmaZipp Game/Scripts/TurnGameObjectOn.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnGameObjectOn : MonoBehaviour
+{
+    [SerializeField]private Transform metalChild;
+    [SerializeField] private bool childIsActive = false;
+   // [SerializeField] private bool setActive;
+
+    private void Awake()
+    {
+        metalChild = transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        /*
+        if (setActive)
+        {
+            metalChild.gameObject.SetActive(true);
+        }
+        else
+        {
+            metalChild.gameObject.SetActive(false);
+        }
+        */
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!childIsActive)
+        {
+            metalChild.gameObject.SetActive(true);
+            childIsActive = true;
+        }
+        else
+        {
+            metalChild.gameObject.SetActive(false);
+            childIsActive = false;
+        }
+        
+    }
+}
